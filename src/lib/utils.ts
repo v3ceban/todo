226d3609089d094acbc7f35fc61b6cdee5924249
Vladimir Ceban <v3ceban@proton.me>
2025-01-03 04:48:34 -0800

    mobile QoL

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,14 +7,15 @@ export function cn(...inputs: ClassValue[]) {
 
 export const nameRegex = /^[a-zA-Z'-]+$/;
 
-export const handleKeyDown = async (e: React.KeyboardEvent, cb: (() => void) | (() => Promise<void>)) => {
-  const isTouchDevice =
-    "ontouchstart" in window || navigator.maxTouchPoints > 0;
-  if (isTouchDevice && e.nativeEvent.isComposing) {
+export const handleKeyDown = async (
+  e: React.KeyboardEvent,
+  cb: (() => void) | (() => Promise<void>),
+) => {
+  if ("ontouchstart" in window || navigator.maxTouchPoints > 0) {
     return;
   }
   if (e.key === "Enter") {
     e.preventDefault();
     await cb();
   }
-}
+};
